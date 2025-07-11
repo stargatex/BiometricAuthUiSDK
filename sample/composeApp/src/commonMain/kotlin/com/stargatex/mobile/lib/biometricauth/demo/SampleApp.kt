7c11bf54a693e71b0661d5e7182effeb8567d0e3
Lahiru Jayawickrama <151607249+Lahiru-J@users.noreply.github.com>
2025-07-11 14:08:20 +0530

    Sample Implementation clear PIN

--- sample/composeApp/src/commonMain/kotlin/com/stargatex/mobile/lib/biometricauth/demo/SampleApp.kt
+++ sample/composeApp/src/commonMain/kotlin/com/stargatex/mobile/lib/biometricauth/demo/SampleApp.kt
@@ -1,6 +1,16 @@
 package com.stargatex.mobile.lib.biometricauth.demo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -52,10 +62,29 @@ fun SampleApp(
             PINKeyX.Compose(
                 platformContextProvider = pinPlatformContextProvider,
                 shouldCheckAvailability = shouldCheckAvailability,
-                onAuthSuccess = onAuthSuccess,
+                onAuthSuccess = {
+                    navController.navigate("dashboard")
+                },
                 onFallback = onFallback,
                 onAuthFailure = onAuthFailure
             )
         }
+
+        composable("dashboard") {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = {
+                    PINKeyX.clearStore(pinPlatformContextProvider)
+                    navController.popBackStack()
+                }) {
+                    Text(modifier = Modifier.wrapContentSize(), text = "Clear Pin")
+                }
+            }
+        }
+
+
     }
 }
